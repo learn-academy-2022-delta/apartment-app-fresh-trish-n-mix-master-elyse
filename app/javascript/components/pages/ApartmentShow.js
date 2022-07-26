fe37f3b09600119fa9b585ba25bf62fbda8bc8e5
LEARN Academy <learnacademy@LEARNs-MacBook-Pro.local>
2022-07-26 16:09:13 -0700

    added apartment show functionality and testing

--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -1,9 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
 
 export default class ApartmentShow extends Component {
   render() {
+    const { apartment } = this.props;
     return (
-      <h3>ApartmentShow</h3>
-    )
+      <>
+        <h3>Your Selection</h3>
+        { apartment && 
+          <Card>
+          <CardImg
+            className="card-img"
+            top
+            width="100%"
+            src={apartment.image}
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle>{apartment.street}</CardTitle>
+            <CardSubtitle>{apartment.city}</CardSubtitle>
+            <CardText>{apartment.state}</CardText>
+            <CardText>{apartment.manager}</CardText>
+            <CardText>{apartment.email}</CardText>
+            <CardText>{apartment.price}</CardText>
+            <CardText>{apartment.bedrooms}</CardText>
+            <CardText>{apartment.bathrooms}</CardText>
+            <CardText>{apartment.pets}</CardText>
+          </CardBody>
+        </Card>}
+      </>
+    );
   }
 }
