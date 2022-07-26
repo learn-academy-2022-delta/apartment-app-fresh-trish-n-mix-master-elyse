f1e31e99cba9ae55ef1188d5175776d9fdff6887
Elyse Montano <elysemontano.dev.com>
2022-07-26 12:13:30 -0700

    adds associations, model spec, and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user1 = User.create(email: 'test@test.com', password: '123testing')
+user2 = User.create(email: 'anothertest@test.com', password: 'testing123')
+
+apartments = [
+  {
+    id: 1,
+    street: '42 Waluby Way',
+    city: 'Sidney',
+    state: 'Austrilia',
+    manager: 'P Sherman',
+    email: 'sherman@apartments.com',
+    price: '$3500',
+    bedrooms: 4,
+    bathrooms: 2,
+    pets: 'fish welcome!',
+    user_id: user1.id
+  },
+  {
+    id: 2,
+    street: '456 Sesame St',
+    city: 'Muppet Land',
+    state: 'CA',
+    manager: 'Big Bird',
+    email: 'bb@apartments.com',
+    price: '$5000 a month',
+    bedrooms: 8,
+    bathrooms: 4,
+    pets: 'muppets only!',
+    user_id: user1.id
+  },
+  {
+    id: 3,
+    street: '123 Main St',
+    city: 'San Diego',
+    state: 'CA',
+    manager: 'Elyse',
+    email: 'elyse@apartments.com',
+    price: '$2000 a month',
+    bedrooms: 3,
+    bathrooms: 2,
+    pets: 'cats and medium sized dogs',
+    user_id: user2.id
+  },
+]
+
+apartments.each do |apartment|
+  Apartment.create apartment
+  puts "creating the apartment: #{apartment}"
+end
--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Apartment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    # I will need to run a create for active record and store it as a variable
+    user1 = User.create(email: 'test@test.com', password: '123testing')
+    apartment = Apartment.create(street: '42 Walaby Way', city: 'Sydney', state: 'Australia', manager: 'P. Sherman', email: 'findingnemo@dentist.com', price: '10 fish', bedrooms: 3, bathrooms: 1, pets: 'fish', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAYL5xCp-hiedGqNFd-Xn-Pd7RPD_Q-A6wUZs6xKAwSW4t9u6Ja0eTkNnxmnySLtmT0bg&usqp=CAU', user_id: user1.id)
+    # Now I want to assert against it
+    expect(apartment).to be_valid
+  end
 end
